42c5ef048d2d416b535b3c868bfe90b4387f7788
Daniel Schulz <daniel.schulz@ocono.de>
2017-03-12 16:06:53 +0100

    Get status once immediately after watcher start

--- cmd/watcher/watcher.go
+++ cmd/watcher/watcher.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	var duration, throttle, bufferTime, offsetTime int
-	flag.IntVar(&duration, "duration", 3600, "This program runs for <throttle> seconds.")
+	flag.IntVar(&duration, "duration", 0, "This program runs for <throttle> seconds.")
 	flag.IntVar(&throttle, "throttle", 60, "This program runs every <throttle> seconds.")
 	flag.IntVar(&bufferTime, "buffer", 600, "This program warns you <buffer> seconds before your line arives.")
 	flag.IntVar(&offsetTime, "offset", 0, "This program assumes that you need <offset> seconds to catch your line.")
@@ -27,6 +27,5 @@ func main() {
 	destination := args[2]
 	transitMode := args[3]
 	lineName := args[4]
-
 	departure.Watch(duration, throttle, bufferTime, offsetTime, apiKey, origin, destination, transitMode, lineName)
 }
--- departure/watcher.go
+++ departure/watcher.go
@@ -6,26 +6,39 @@ import (
 )
 
 func Watch(duration, throttle, bufferTime, offsetTime int, apiKey, origin, destination, transitMode, lineName string) {
+	printStatus(bufferTime, offsetTime, apiKey, origin, destination, transitMode, lineName)
+
 	ticker := time.NewTicker(time.Second * time.Duration(throttle))
 	go func() {
 		for range ticker.C {
-			desiredDepTime := time.Now().Add(time.Duration(offsetTime) * time.Second)
-			depTime, err := GetDepartureTime(origin, destination, apiKey, transitMode, lineName, desiredDepTime)
-			if err != nil {
-				fmt.Printf("ERROR %s\n", err)
-			} else {
-				until := time.Until(depTime)
-				untilSeconds := int(until.Seconds()) - offsetTime
-
-				if untilSeconds < bufferTime {
-					fmt.Printf("GO %d\n", untilSeconds)
-				} else {
-					fmt.Printf("WAIT %d\n", untilSeconds)
-				}
-			}
+			printStatus(bufferTime, offsetTime, apiKey, origin, destination, transitMode, lineName)
 		}
 	}()
+
 	time.Sleep(time.Second * time.Duration(duration))
 	ticker.Stop()
+
 	fmt.Println("DONE")
+}
+
+func printStatus(bufferTime, offsetTime int, apiKey, origin, destination, transitMode, lineName string) {
+	status := getStatus(bufferTime, offsetTime, apiKey, origin, destination, transitMode, lineName)
+	println(status)
+}
+
+func getStatus(bufferTime, offsetTime int, apiKey, origin, destination, transitMode, lineName string) string {
+	desiredDepTime := time.Now().Add(time.Duration(offsetTime) * time.Second)
+	depTime, err := GetDepartureTime(origin, destination, apiKey, transitMode, lineName, desiredDepTime)
+	if err != nil {
+		return fmt.Sprintf("ERROR %s", err)
+	} else {
+		until := time.Until(depTime)
+		untilSeconds := int(until.Seconds()) - offsetTime
+
+		if untilSeconds < bufferTime {
+			return fmt.Sprintf("GO %d", untilSeconds)
+		} else {
+			return fmt.Sprintf("WAIT %d", untilSeconds)
+		}
+	}
 }
